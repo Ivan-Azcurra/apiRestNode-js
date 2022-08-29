71a6dfb094fddd1441e7b8bb3878f11af61cecb3
azcurrita93 <chepes_ivan@hotmail.com>
2022-08-29 01:44:03 -0300

    metodo post/crear implementado

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var mysql = require("mysql");
 
 var app = express();
 
+// Con esto le decimos a la aplición que vamos a usar JSON.
+app.use(express.json());
+
 // Establecemos los párametros de conexión con la DB
 var conexion = mysql.createConnection({
   host: "localhost",
@@ -42,11 +45,14 @@ app.get("/api/articulos", (req, res) => {
 
 // mostrar un SOLO artículo mediante "id", se agrega a la ruta el "/:id"
 app.get("/api/articulos/:id", (req, res) => {
-    // "query" consulta a la DB
-    // en la sentencion de abajo debemos usar la clausula "WHERE"
-    // cuando en la ruta coloquemos un id, el mismo va a ocupar el lugar de "?"
-    // req.params = es el parametro que especificamos en la ruta, en este caso es "/:id"
-    conexion.query("SELECT * FROM articulos WHERE id = ?", [req.params.id], (error, fila) => {
+  // "query" consulta a la DB
+  // en la sentencion de abajo debemos usar la clausula "WHERE"
+  // cuando en la ruta coloquemos un id, el mismo va a ocupar el lugar de "?"
+  // req.params = es el parametro que especificamos en la ruta, en este caso es "/:id"
+  conexion.query(
+    "SELECT * FROM articulos WHERE id = ?",
+    [req.params.id],
+    (error, fila) => {
       if (error) {
         // si hay un error lo capturamos y lo mostramos "throw"
         throw error;
@@ -54,21 +60,31 @@ app.get("/api/articulos/:id", (req, res) => {
         // res.send(fila);
         res.send(fila[0].descripcion);
       }
-    });
+    }
+  );
+});
+
+// metodo crear articulo
+app.post("/api/articulos", (req, res) => {
+  //dentro de este obj van a estar todos los valores que le vamos a mandar la sentencia sql.
+  // los valores de las "keys - llaves", se van a cargar desde postman con el parametro "req" -body = son los datos cargados en el cuerpo de la petición --body. y el valor cargado en postman.
+  let data = {
+    descripcion: req.body.descripcion,
+    precio: req.body.precio,
+    stock: req.body.stock,
+  };
+
+  let sql = "INSERT INTO articulos SET ?";
+  // sentencia sql para crear un nuevo articulo en la tabla "articulos" / SET, para setear los campos a llenar. Con "?" definimos los los parametros (descripcion, precio, stock)
+  conexion.query(sql, data, function (error, results) {
+    if (error) {
+      // si hay un error lo capturamos y lo mostramos "throw"
+      throw error;
+    } else {
+      res.send(results);
+    }
   });
-
-  app.post('/api/articulos', (req, res)=>{
-    //dentro de este obj van a estar todos los valores que le vamos a mandar la sentencia sql.
-      let data = {
-    // los valores de las "keys - llaves", se van a cargar desde postman con el parametro "req" -body = son los datos cargados en el cuerpo de la petición --body. y el valor cargado en postman. 
-        descripcion:req.body.descripcion, 
-        precio:req.body.precio, 
-        stock:req.body.stock}
-    conexion.query(sql, data, function(error, results){
-
-    });
-  });
-
+});
 
 // Variable de entorno (al parece se definen en MAYUSCULAS)
 const puerto = process.env.PUERTO || 3000; //se va ejecutar en el puerto 7000, si está disponible de lo contrario lo hará en el 3000.
