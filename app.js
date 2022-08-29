4d7b62e68de612f7c928e1f963ff38369a898841
azcurrita93 <chepes_ivan@hotmail.com>
2022-08-29 01:22:36 -0300

    implementando metodo post/crear articulo en DB

--- app.js
+++ app.js
@@ -26,6 +26,50 @@ app.get("/", function (req, res) {
   res.send("Ruta INICIO");
 });
 
+// me faltaba un "/" en la ruta del get.
+// mostrar todos los articulos.
+app.get("/api/articulos", (req, res) => {
+  // "query" consulta a la DB
+  conexion.query("SELECT * FROM articulos", (error, filas) => {
+    if (error) {
+      // si hay un error lo capturamos y lo mostramos "throw"
+      throw error;
+    } else {
+      res.send(filas);
+    }
+  });
+});
+
+// mostrar un SOLO artículo mediante "id", se agrega a la ruta el "/:id"
+app.get("/api/articulos/:id", (req, res) => {
+    // "query" consulta a la DB
+    // en la sentencion de abajo debemos usar la clausula "WHERE"
+    // cuando en la ruta coloquemos un id, el mismo va a ocupar el lugar de "?"
+    // req.params = es el parametro que especificamos en la ruta, en este caso es "/:id"
+    conexion.query("SELECT * FROM articulos WHERE id = ?", [req.params.id], (error, fila) => {
+      if (error) {
+        // si hay un error lo capturamos y lo mostramos "throw"
+        throw error;
+      } else {
+        // res.send(fila);
+        res.send(fila[0].descripcion);
+      }
+    });
+  });
+
+  app.post('/api/articulos', (req, res)=>{
+    //dentro de este obj van a estar todos los valores que le vamos a mandar la sentencia sql.
+      let data = {
+    // los valores de las "keys - llaves", se van a cargar desde postman con el parametro "req" -body = son los datos cargados en el cuerpo de la petición --body. y el valor cargado en postman. 
+        descripcion:req.body.descripcion, 
+        precio:req.body.precio, 
+        stock:req.body.stock}
+    conexion.query(sql, data, function(error, results){
+
+    });
+  });
+
+
 // Variable de entorno (al parece se definen en MAYUSCULAS)
 const puerto = process.env.PUERTO || 3000; //se va ejecutar en el puerto 7000, si está disponible de lo contrario lo hará en el 3000.
 
